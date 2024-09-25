d84e2cfc8134b23e7ee2e4ed1660f736409c1918
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2024-09-25 19:10:08 +0500

    Changed styles added options button and made beatifull scrollbar

---
+++ src/Client/CustomMessengerBlazor/wwwroot/js/nicescroll.js
@@ -0,0 +1,10 @@
+﻿// wwwroot/js/nicescroll.js
+function initializeNiceScroll(selector) {
+    $(selector).niceScroll({
+        cursorcolor: "#424242", // Customize the cursor color
+        cursorwidth: "8px",     // Customize the cursor width
+        cursorborder: "none",   // Remove cursor border
+        railpadding: { top: 0, right: 0, left: 0, bottom: 0 }, // Remove extra padding
+        autohidemode: false     // Ensure the scrollbar is always visible (optional)
+    });
+}
